7005671731f434e6e5416fe73d7f6729db81ee2e
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-07-01 21:56:12 +0530

    update portfolio_leading_footer.dart

--- lib/screens/portfolio/portfolio_leading/elements/portfolio_leading_footer.dart
+++ lib/screens/portfolio/portfolio_leading/elements/portfolio_leading_footer.dart
@@ -13,13 +13,13 @@ class PortfolioLeadingFooter extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        PortfolioLeadingFooterIcon( icon: Image.network('https://img.icons8.com/color/48/000000/github.png', height: 20, width: 20), onTap: () async { final url = Uri.parse('https://github.com/fazil-kp'); await launchUrl(url);}, ),
+        PortfolioLeadingFooterIcon(icon: Image.network('https://img.icons8.com/color/48/000000/github.png', height: 20, width: 20), onTap: () async { final url = Uri.parse('https://github.com/fazil-kp'); await launchUrl(url);},),
         10.width,
-        PortfolioLeadingFooterIcon( icon: Image.network('https://img.icons8.com/color/48/000000/linkedin.png', height: 20, width: 20), onTap: () async { final url = Uri.parse('https://www.linkedin.com/in/fazil-kp-061459235/'); await launchUrl(url); }, ),
+        PortfolioLeadingFooterIcon(icon: Image.network('https://img.icons8.com/color/48/000000/linkedin.png', height: 20, width: 20), onTap: () async { final url = Uri.parse('https://www.linkedin.com/in/fazil-kp-061459235/'); await launchUrl(url); },),
         10.width,
-        PortfolioLeadingFooterIcon( icon: Image.network("https://upload.wikimedia.org/wikipedia/commons/6/65/HackerRank_logo.png", height: 20, width: 20), onTap: () async { final url = Uri.parse('https://www.hackerrank.com/profile/mfazilkp7'); await launchUrl(url); }, ),
+        PortfolioLeadingFooterIcon(icon: Image.network("https://upload.wikimedia.org/wikipedia/commons/6/65/HackerRank_logo.png", height: 20, width: 20), onTap: () async { final url = Uri.parse('https://www.hackerrank.com/profile/mfazilkp7'); await launchUrl(url); },),
         10.width, 
-        PortfolioLeadingFooterIcon( icon: Image.network('https://img.icons8.com/color/48/000000/youtube.png', height: 20, width: 20), onTap: () async { final url = Uri.parse('https://www.youtube.com/channel/UCVqYXdZZAwP2dkEDPHXVgSA'); await launchUrl(url); }, ),
+        PortfolioLeadingFooterIcon(icon: Image.network('https://img.icons8.com/color/48/000000/youtube.png', height: 20, width: 20), onTap: () async { final url = Uri.parse('https://www.youtube.com/channel/UCVqYXdZZAwP2dkEDPHXVgSA'); await launchUrl(url); },),
       ],
     );
   }
