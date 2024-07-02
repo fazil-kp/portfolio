901b13ceb807fa5cd03885b55c6b70219041040d
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-07-02 20:10:40 +0530

    update portfolio_trailing.dart

--- lib/screens/portfolio/portfolio_trailing/portfolio_trailing.dart
+++ lib/screens/portfolio/portfolio_trailing/portfolio_trailing.dart
@@ -59,6 +59,12 @@ class PortfolioTrailing extends ConsumerWidget {
             ],
           ),
           10.height,
+
+
+
+
+
+          
           Expanded(
             child: IndexedStack(
               index: ref.watch(portfolioVM).pages,
