874b91d69afed854a32ebba63ee80b3d093601b0
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-07-02 20:21:46 +0530

    update portfolio_trailing.dart

--- lib/screens/portfolio/portfolio_trailing/portfolio_trailing.dart
+++ lib/screens/portfolio/portfolio_trailing/portfolio_trailing.dart
@@ -15,9 +15,7 @@ class PortfolioTrailing extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: PortColor.primary),
-      height: context.height() - 100,
+    return Container( decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: PortColor.primary), height: context.height() - 100,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
@@ -26,45 +24,13 @@ class PortfolioTrailing extends ConsumerWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 20, top: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(ref.watch(portfolioVM).titles[ref.watch(portfolioVM).pages], style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: PortColor.white, fontSize: 20, fontWeight: FontWeight.bold)),
-                    5.height,
-                    Container(height: 5, width: 50, decoration: BoxDecoration(color: PortColor.secondary, borderRadius: BorderRadius.circular(10))),
-                  ],
-                ),
-              ),
+              Padding( padding: const EdgeInsets.only(left: 20, top: 20), child: Column( crossAxisAlignment: CrossAxisAlignment.start, mainAxisAlignment: MainAxisAlignment.start, children: [ Text(ref.watch(portfolioVM).titles[ref.watch(portfolioVM).pages], style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: PortColor.white, fontSize: 20, fontWeight: FontWeight.bold)), 5.height, Container(height: 5, width: 50, decoration: BoxDecoration(color: PortColor.secondary, borderRadius: BorderRadius.circular(10))), ], ), ),
               const Spacer(),
-              Container(
-                height: 55,
-                width: MediaQuery.of(context).size.width * 0.4,
-                decoration: BoxDecoration(color: PortColor.secondary.withOpacity(0.5), borderRadius: const BorderRadius.only(topRight: Radius.circular(10), bottomLeft: Radius.circular(10))),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 30, right: 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      HoverText(text: "About", onTap: () => {ref.watch(portfolioVM).pages = 0, ref.watch(portfolioVM).refresh()}, index: 0),
-                      HoverText(text: "Services", onTap: () => {ref.watch(portfolioVM).pages = 1, ref.watch(portfolioVM).refresh()}, index: 1),
-                      HoverText(text: "Projects", onTap: () => {ref.watch(portfolioVM).pages = 2, ref.watch(portfolioVM).refresh()}, index: 2),
-                      HoverText(text: "Contact", onTap: () => {ref.watch(portfolioVM).pages = 3, ref.watch(portfolioVM).refresh()}, index: 3),
-                    ],
-                  ),
-                ),
-              ),
+              Container( height: 55, width: MediaQuery.of(context).size.width * 0.4, decoration: BoxDecoration(color: PortColor.secondary.withOpacity(0.5), borderRadius: const BorderRadius.only(topRight: Radius.circular(10), bottomLeft: Radius.circular(10))), child: Padding( padding: const EdgeInsets.only(left: 30, right: 30), child: Row( mainAxisAlignment: MainAxisAlignment.spaceAround, children: [ HoverText(text: "About", onTap: () => {ref.watch(portfolioVM).pages = 0, ref.watch(portfolioVM).refresh()}, index: 0), HoverText(text: "Services", onTap: () => {ref.watch(portfolioVM).pages = 1, ref.watch(portfolioVM).refresh()}, index: 1), HoverText(text: "Projects", onTap: () => {ref.watch(portfolioVM).pages = 2, ref.watch(portfolioVM).refresh()}, index: 2), HoverText(text: "Contact", onTap: () => {ref.watch(portfolioVM).pages = 3, ref.watch(portfolioVM).refresh()}, index: 3), ], ), ), ),
             ],
           ),
           10.height,
-          Expanded(
-            child: IndexedStack(
-              index: ref.watch(portfolioVM).pages,
-              children: const [About(), Service(), Project(), Contact()],
-            ),
-          ),
+          Expanded(child: IndexedStack(index: ref.watch(portfolioVM).pages, children: const [About(), Service(), Project(), Contact()])),
         ],
       ),
     );
